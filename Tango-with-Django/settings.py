3a99021e5584be23643a1c3c6517488a46fe15bf
Ken Li <iliawnek@gmail.com>
2016-01-26 19:38:06 +0000

    More name changes

--- Tango-with-Django/settings.py
+++ Tango-with-Django/settings.py
@@ -48,9 +48,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'Rango2.urls'
+ROOT_URLCONF = 'Tango-with-Django.urls'
 
-WSGI_APPLICATION = 'Rango2.wsgi.application'
+WSGI_APPLICATION = 'Tango-with-Django.wsgi.application'
 
 
 # Database
--- Tango-with-Django/urls.py
+++ Tango-with-Django/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'Rango2.views.home', name='home'),
+    # url(r'^$', 'Tango-with-Django.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
