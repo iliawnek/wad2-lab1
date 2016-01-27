143cb62b50a8f613df6b674daa86a8659d95b74d
Ken Li <iliawnek@gmail.com>
2016-01-27 00:07:42 +0000

    Add about page

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("rango says hey there world!")
+    return HttpResponse("Rango says hey there world! <a href='/rango/about'>Click here to find more about me.</a>")
+
+
+def about(request):
+    return HttpResponse("This sentence is about rango! If don't like it here, <a href='/rango'>click this.</a>")
