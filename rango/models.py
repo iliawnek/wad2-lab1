905727eecf25a313e13fea958b55fb353f0b1984
Ken Li <iliawnek@gmail.com>
2016-01-28 19:23:56 +0000

    Populated database using custom script

--- rango/models.py
+++ rango/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = "categories"
 
     def __unicode__(self):
         return self.name
