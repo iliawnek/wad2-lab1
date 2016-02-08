25427a75717e8f28128ce68e3a12c3c09ba7c3d8
Ken Li <iliawnek@gmail.com>
2016-02-08 17:08:33 +0000

    Set up category and page forms

---
+++ rango/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from rango.models import *
+
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, help_text="category name")
+    # views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    # likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    class Meta:
+        model = Category
+        fields = ('name',)
+
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, help_text="page title")
+    url = forms.URLField(help_text="URL")
+    # views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        model = Page
+        exclude = ('category',)
