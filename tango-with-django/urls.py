4af9071b513c11f05d42d5dc295e06b47bff67f5
Ken Li <iliawnek@gmail.com>
2016-03-06 20:02:54 +0000

    Successful registration now redirects straight to home page

--- tango-with-django/urls.py
+++ tango-with-django/urls.py
@@ -1,11 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
+from registration.backends.simple.views import RegistrationView
+
+
+class RangoRegistrationView(RegistrationView):
+    def get_success_url(self, user=None):
+        return '/rango/'
+
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^rango/', include('rango.urls')),
-                       url(r'^accounts/', include('registration.backends.simple.urls'))
+                       url(r'^accounts/register/$', RangoRegistrationView.as_view(), name='registration_register'),
+                       url(r'^accounts/', include('registration.backends.simple.urls')),
                        )
 
 if settings.DEBUG:
